ac30f58febd796d4a2ec2e061d30f7cc5c3a4de0
JordanHolberton <jordan.chatillon76@laposte.net>
2024-09-02 14:32:37 +0200

    add task 8

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str1 = "Python"; str2 = "object-oriented programming"
+str = str2 + " " + str1
+print(str)
