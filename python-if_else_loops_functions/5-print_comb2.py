f77e2dbea1ae3cc098e2a5a945519e4d4a511796
JordanHolberton <jordan.chatillon76@laposte.net>
2024-09-03 15:16:04 +0200

    fix

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for i in range (100):
-    print("{:02}".format(i), end=", ")
-    if i == 99:
-        print("{}" .format(i))
+for i in range(100):
+    print("{:02}".format(i), end=", " if i < 99 else "\n")
