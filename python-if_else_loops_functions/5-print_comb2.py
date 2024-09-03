401a52ff0c6da5d77d40cccaf7a535297d217406
JordanHolberton <jordan.chatillon76@laposte.net>
2024-09-03 15:01:29 +0200

    fix picodingstyle

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range (100):
-	print("{:02}".format(i), end=", ")
-	if i == 99:
-		print(f"{i}")
+for i in range(100):
+    print("{:02}".format(i), end=", ")
+    if i == 99:
+        print(i)
