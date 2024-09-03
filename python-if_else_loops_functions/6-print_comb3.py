8109d89cbb8e4910de2b02b02cbd2c710939d01a
JordanHolberton <jordan.chatillon76@laposte.net>
2024-09-03 15:17:45 +0200

    fix last task

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        if i != 0 or j != 1:
-            print(", ", end="")
+        # Print the combination of two digits
         print(f"{i}{j}", end="")
+        # Add a comma and space after every combination except the last one
+        if i != 8 or j != 9:
+            print(", ", end="")
 print()
