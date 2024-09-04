d679590ea935ff0e8b01eed132549968674a3e9e
JordanHolberton <jordan.chatillon76@laposte.net>
2024-09-04 11:22:20 +0200

    second attempt task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if argc == 0:
         print("0 arguments.")
     elif argc == 1:
-        print("1 argument:")
+        print("{} argument:".format(argc, end=""))
     else:
         print("{} arguments:".format(argc))
     for i in range(argc):
