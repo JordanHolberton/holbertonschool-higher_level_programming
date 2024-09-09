f4307e78ea2b8398b70f653b4ee8dea867b4759f
JordanHolberton <jordan.chatillon76@laposte.net>
2024-09-09 13:35:52 +0200

    fix task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple = ()
-    if tuple == 0:
-        tuple = 0, "None"
-    tuple = len(sentence), sentence[0]
-    return tuple
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
