20835e4a08c347ac6cc3c584d35efa50ca60b44a
JordanHolberton <jordan.chatillon76@laposte.net>
2024-09-10 13:01:56 +0200

    Write a script for conver from roman number to numerical number

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string
+    if not roman_string:
         return 0
 
-roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
 
-    for i in roman_number:
-        if 
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        current_value = roman.get(char, 0)
+
+        if current_value >= prev_value:
+            total += current_value
+        else:
+            total -= current_value
+    
+        prev_value = current_value
+
+    return total
