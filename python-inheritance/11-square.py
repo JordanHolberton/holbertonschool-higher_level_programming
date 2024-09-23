63bed019033ca9b2209265ebf75e9de06e4057df
JordanHolberton <jordan.chatillon76@laposte.net>
2024-09-23 14:35:39 +0200

    fix all my problems

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,17 +1,77 @@
 #!/usr/bin/python3
-"""Define Rectangle subclass Square"""
-Rectangle = __import__('9-rectangle').Rectangle
+"""defines a base class for geometry-related classes."""
+
+
+class BaseGeometry:
+    """A base class for geometry operations."""
+
+    def area(self):
+        """Calculate the area of the geometry.
+
+        Raises:
+            Exception: Always raises an exception with the message
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that a value is a positive integer.
+
+        Args:
+            name (str): The name of the value being validated.
+            value: The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class, inherits from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializes a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[{}] {}/{}".format(
+            self.__class__.__name__, self.__width, self.__height
+            )
 
 
 class Square(Rectangle):
-    """Represent a Square"""
+    """
+    A class representing a square, inheriting from Rectangle.
+    """
 
     def __init__(self, size):
-        """Initialize a new square
+        """
+        Initialize a Square instance.
 
         Args:
-            size (int) : The size of the new square
+            size (int): The size of the square's sides.
+
+        Raises:
+
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self.size = size
+        self.__size = size
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,41 +1,20 @@
 #!/usr/bin/python3
-"""Defines an empty class BaseGeometry."""
-
-
-class BaseGeometry:
-    """Represent base geometry."""
-
-    def area(self):
-        """Exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+"""Defines a Rectangle class that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Represente a rectangle using BaseGeomety"""
+    """Rectangle class, inherits from BaseGeometry."""
 
     def __init__(self, width, height):
-        """Intialize a new Rectangle.
+        """
+        Initialize a Rectangle instance.
 
         Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
         """
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
