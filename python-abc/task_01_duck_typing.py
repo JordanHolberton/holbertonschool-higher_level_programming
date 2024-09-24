199abe1619a64598d79ffbdb371392a5cbbeb918
JordanHolberton <jordan.chatillon76@laposte.net>
2024-09-24 14:21:02 +0200

    try to fix task 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -19,30 +19,29 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError("Radius must be greater than 0")
         self.radius = radius
 
     def area(self):
-        """Calculate the area of the circle"""
         return math.pi * self.radius ** 2
 
     def perimeter(self):
-        """Calculate the perimeter (circumference) of the circle"""
         return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
     def __init__(self, width, height):
+        if width <= 0 or height <= 0:
+            raise ValueError("Width and Height must be greater than 0")
         self.width = width
         self.height = height
 
     def area(self):
-        """Calculate the area of the rectangle"""
         return self.width * self.height
 
     def perimeter(self):
-        """Calculate the perimeter of the rectangle"""
         return 2 * (self.width + self.height)
-
 
 def shape_info(shape):
     """Prints the area and perimeter of the shape passed"""
