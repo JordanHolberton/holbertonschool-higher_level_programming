4add18365a3a50458f69a4ff421f5a6029c8a56b
JordanHolberton <jordan.chatillon76@laposte.net>
2024-10-14 09:35:52 +0200

    delete ","

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import JWTManager, create_access_token,
+from flask_jwt_extended import JWTManager, create_access_token
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 # Initialize Flask application
