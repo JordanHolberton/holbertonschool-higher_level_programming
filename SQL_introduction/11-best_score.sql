c8b8d2cdd0562abd683ce4f39ef4e2a3a773cf03
JordanHolberton <jordan.chatillon76@laposte.net>
2024-10-28 13:43:25 +0100

    Add script for list best score => 10

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
