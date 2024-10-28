0e8f821bb8a2800763476356544d92f0bb0f0b2d
JordanHolberton <jordan.chatillon76@laposte.net>
2024-10-28 14:06:07 +0100

    Update task 12

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,2 @@
--- This is a comment
-UPDATE second_table
-SET score = 10
-WHERE name = Bob;
+-- No cheating! You can't use SELECT in this exercise. You can only use UPDATE.
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
