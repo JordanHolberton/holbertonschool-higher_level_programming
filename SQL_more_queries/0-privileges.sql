3fd6d1fb14a5297bcb503c71cad7788f86439560
JordanHolberton <jordan.chatillon76@laposte.net>
2024-10-29 09:46:54 +0100

    commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Content: Privileges
-SHOW GRANTS FOR 'user_0d1'@'localhost';
-SHOW GRANTS FOR 'user_0d2'@'localhost';
+-- Content: Queries to show privileges of users
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
