66b38b66aa8906929c5bf009efe29292d0b5c72f
JordanHolberton <jordan.chatillon76@laposte.net>
2024-10-30 11:16:47 +0100

    fix doc again

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
+""" Lists all states from the database hbtn_0e_0_usa """
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     """ Lists all states from the database hbtn_0e_0_usa
+
         Usage: ./0-select_states.py <mysql username>
         <mysql password> <database name>
 
