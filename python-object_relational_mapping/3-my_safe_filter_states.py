473e045d90aa5bfdad97d2bc535652497e9daf5a
JordanHolberton <jordan.chatillon76@laposte.net>
2024-10-30 13:19:43 +0100

    update task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 """
-script that takes in an argument and displays all values in the states table of
-hbtn_0e_0_usa where name matches the argument.
+ SQL injection safe script that takes in an argument and displays all values
 """
 
 import sys
@@ -18,7 +17,7 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name = %s \
-                   ORDER BY id ASC".format(sys.argv[4]))
+        ORDER BY id ASC", (sys.argv[4],))
     states = cursor.fetchall()
     for state in states:
         if state[1] == sys.argv[4]:
