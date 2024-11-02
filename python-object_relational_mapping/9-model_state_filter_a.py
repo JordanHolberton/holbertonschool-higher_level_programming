b67ac9dd1feaf7857b6cf15516a850126ea8f428
JordanHolberton <jordan.chatillon76@laposte.net>
2024-11-02 21:48:39 +0100

    Refactor script to filter cities by state and improve code style

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-""" script that prints the first State object
-from the database hbtn_0e_6_usa"""
+"""script that lists all State objects that contain the letter
+a from the database hbtn_0e_6_usa"""
 
 import sys
-from model_state import Base, State
+from model_state import State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -12,15 +12,13 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(username, password, database))
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).order_by(State.id).first()
-    if state:
+    states = session.query(State).filter(State.name.like('%a%'))\
+                                 .order_by(State.id).all()
+    for state in states:
         print("{}: {}".format(state.id, state.name))
-    else:
-        print("Nothing")
     session.close()
