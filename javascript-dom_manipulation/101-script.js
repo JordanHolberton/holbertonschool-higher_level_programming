88d5f71393b9eb917b455a23cc0261b6baceb18b
JordanHolberton <jordan.chatillon76@laposte.net>
2024-11-19 12:07:06 +0100

    Add script for language translation using external API

---
+++ javascript-dom_manipulation/101-script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('btn_translate').addEventListener('click', function() {
+        const languageCode = document.getElementById('language_code').value;
+        fetch(`https://hellosalut.stefanbohacek.dev/?lang=${languageCode}`)
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('hello').textContent = data.hello;
+            })
+            .catch(error => console.error('Error:', error));
+    });
+});
